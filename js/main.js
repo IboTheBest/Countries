d4caacb7af77e93746ea6ea31d6851144b296829
IboTheBest <cybergenyus@gmail.com>
2024-12-03 09:03:18 +0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -9,9 +9,20 @@ function renderCountryes(array, list){
         elCountryItem.innerHTML = `
         <img class="w-full h-[160px] object-cover" src="${item.flag}" alt="${item.name}" width="264" height="160">
         <div class="p-6">
-            <h2 class="text-[#111517] mb-4 text-[18px] leading-[26px] font-extrabold">${item.name}</h2>
-            <p class="font-bold text-[14] mb-2 leading 4">Population: <span class="font-extralight text-[14] leading 4">${item.population}</span></p>
-            <p class="font-bold text-[14] leading 4">Capital: <span class="font-extralight text-[14] leading 4">${item.capital}</span></p>
+            <h2 class="count-title text-[#111517] mb-4 text-[18px] leading-[26px] font-extrabold">${item.name}</h2>
+            <p class="count-info font-bold text-[14] mb-2 leading 4">Population: <span class="font-extralight text-[14] leading 4">${item.population}</span></p>
+            <p class="count-info font-bold text-[14] leading 4">Capital: <span class="font-extralight text-[14] leading 4">${item.capital}</span></p>
+        </div>
+                <div class="flex justify-between  p-5">
+            <button class="rounded-full border-[3px] border-black w-[35px] h-[35px] flex justify-center items-center ">
+                <img src="./images/liked.png" alt="" width="25" >
+            </button>
+            <button class="rounded-full border-[3px] border-black w-[35px] h-[35px] flex justify-center items-center ">
+                <img src="./images/more.png" alt="" width="25" >
+            </button>
+            <button class="rounded-full border-[3px] border-black w-[35px] h-[35px] flex justify-center items-center ">
+                <img src="./images/saved.png" alt="" width="25" >
+            </button>
         </div>
         `        
         elHeroList.append(elCountryItem)
@@ -51,4 +62,18 @@ elSearch.addEventListener("input", function(event){
 function changeImg(){
     let butn =  document.querySelector(".dark")
     butn.src = './images/brightness.png'
+    document.body.className = "bg-[#202C36]"
+    let elHeader = document.querySelector(".header")
+    elHeader.classList.add("bg-[#2B3844]")
+    elSearch.classList.add("bg-[#2B3844]")
+    elSelect.classList.add("bg-[#2B3844]") 
+    let eGeneralTitle =  document.querySelector(".general-title")
+    eGeneralTitle.classList.add("text-white") 
+    let elCountryItem = document.querySelectorAll("li")
+    elCountryItem.toggle("make-dark")
+    let countryTitle = document.querySelectorAll("count-title")
+    let countyInfo = document.querySelectorAll(".count-info")
+    countryTitle.toggle("make-text-dark")
+    countyInfo.toggle("make-text-dark")
+
 }
